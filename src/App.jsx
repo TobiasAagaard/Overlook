e42762be17d04ccc48bfbbe0ca5262eedc6e92a0
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-02-06 22:46:58 +0100

    Routes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { BrowserRouter } from "react-router-dom"
-import { AppRouter } from "./components/AppRouter/AppRouter"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MainLayout } from "./Layout/MainLayout";
+import { Frontpage } from "./pages/Frontpage/Frontpage";
+import { Destinations } from "./pages/Destinations/Destinations";
+import { Resevation } from "./pages/Resevation/Resevation";
+import { Rooms } from "./pages/Rooms/Rooms";
+import { Login } from "./pages/Login/Login";
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Frontpage />} />
+            <Route path="/Destinations" element={<Destinations />} />
+            <Route path="/Resevation" element={<Resevation />} />
+            <Route path="/Rooms" element={<Rooms />} />
+            <Route path="/Login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Nav bar</li>
+            </ul>
+        </nav>
+    )
+}
