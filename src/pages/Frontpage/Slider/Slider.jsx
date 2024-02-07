e3ef832e56e450aa8a75f207f782c916cb2274f0
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-02-07 23:15:14 +0100

    Fix slider

--- src/pages/Frontpage/Slider/Slider.jsx
+++ src/pages/Frontpage/Slider/Slider.jsx
@@ -2,7 +2,6 @@ import Carousel from "react-material-ui-carousel";
 import  ImageSlider  from "./ImageSlider.js";
 
 const Item = (props) => {
-    // Retunere images fra ImageSlider.js
     return (
       <img
         src={props.item.images}
@@ -14,23 +13,16 @@ const Item = (props) => {
   
   const HeroSlider = () => {
     const carouselSettings = {
-      // Animation til karusellen
       animation: "fade",
-      // Kontrollér, hvor hyppigt karusellen skal skifte.
-      interval: 3000,
-      // Sæt dette til sandt for at skjule pilene.
+      interval: 500,
       navButtonsAlwaysInvisible: true
     };
   
   
     return (
       <div className="slider-container">
-        {/* Returnere Carousel-komponent fra react-material-ui-carousel biblioteket */}
         <Carousel {...carouselSettings}>
-          {/* Mapper igennem ImageSlider og viser hvert element som en Item-komponent */}
           {ImageSlider.map((data) => (
-            // For hvert objekt i ImageSlider, opretter denne kode en Item-komponent
-            // med det rigtige billede og data, så de kan vises karusellen.
             <Item key={data.id} item={data} />
           ))}
         </Carousel>
