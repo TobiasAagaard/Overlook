acebc0ef8a8d1ea5a6854e37e1de9bbfe8413596
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-02-08 04:48:45 +0100

    News

--- src/pages/Frontpage/Frontpage.jsx
+++ src/pages/Frontpage/Frontpage.jsx
@@ -1,12 +1,14 @@
 import { ContentWrapper } from "../../components/ContentWrapper/ContentWrapper";
 import { HeroSlider } from "./Slider/Slider.jsx";
 import { Header } from "../../components/Header/Header.jsx";
+import { NewsCards } from "./News/NewsCards.jsx";
 
 export const Frontpage = () => {
     return (
-        <ContentWrapper  title="Forside | VELKOMMEN TIL HOTEL OVERLOOK ONLINE">
+        <ContentWrapper  title="VELKOMMEN TIL HOTEL OVERLOOK ONLINE">
             <Header header="VELKOMMEN TIL HOTEL OVERLOOK ONLINE" />
             <HeroSlider />
+            <NewsCards />
         </ContentWrapper>
     )
 }
---
+++ src/pages/Frontpage/News/NewsCards.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Styles from "./NewsCards.module.scss"
+
+export const NewsCards = () => {
+    const [news, setNews] = useState([])
+
+    useEffect(() => {
+        const endpoint = "http://localhost:4000/news"
+        const getNews = async () => {
+            try {
+                const res = await axios(endpoint);
+                console.log(res)
+                setNews(res.data)
+            } catch (err) {
+                console.error(err)
+            }
+        };
+        getNews()
+    }, [])
+    return (
+       <>
+       <h2>Sidste nyt</h2>
+        <figure className={Styles.newsCards}>
+            {news.slice(0, 3).map((data)=> (
+                <div key={data.id}>
+                    <img src={`http://localhost:4000/images/${data.image.filename}`} alt={data.title} />
+                    <figcaption>
+                        <h3>{data.title}</h3>
+                        <p>{data.teaser}</p>
+                        </figcaption>
+                </div>
+            ))}
+        </figure>
+       </>
+    )
+}
